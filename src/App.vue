7798be5df5b325315b72f37a4652e88c610215b8
Jessica Schäfer <jschaefer@3pc.de>
2021-11-07 15:04:19 +0100

    feat(EventCard): getEvents with axios in EventService

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,23 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">EventList</router-link> |
     <router-link to="/about">About</router-link>
   </div>
   <router-view />
 </template>
 
 <style>
-#app {
+.vue-app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: black;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+a,
+a:visited {
+  color: black;
+  text-decoration: none;
 }
 </style>
---
+++ src/components/EventCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="event-card">
+    <span> @ {{ event.time }} on {{ event.date }}</span>
+    <h2>{{ event.title }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EventCard',
+  props: {
+    event: Object,
+  },
+}
+</script>
+
+<style scoped>
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+</style>
