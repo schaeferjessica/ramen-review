9e5b03c7aefee2e486b6ad89d68d5672ffdcad89
Jessica Schäfer <jschaefer@3pc.de>
2021-11-07 17:35:39 +0100

    feat(EventList): add Pagination with props in route

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,31 @@
-import { createRouter, createWebHistory } from "vue-router";
-import EventList from "../views/EventList.vue";
-import EventDetail from "../views/EventDetail.vue";
-import About from "../views/About.vue";
+import { createRouter, createWebHistory } from 'vue-router'
+import EventList from '../views/EventList.vue'
+import EventDetail from '../views/EventDetail.vue'
+import About from '../views/About.vue'
 
 const routes = [
   {
-    path: "/",
-    name: "EventList",
+    path: '/',
+    name: 'EventList',
     component: EventList,
+    props: (route) => ({ page: parseInt(route.query.page) || 1 }),
   },
   {
-    path: "/event/:id",
-    name: "EventDetail",
+    path: '/event/:id',
+    name: 'EventDetail',
     props: true,
     component: EventDetail,
   },
   {
-    path: "/about",
-    name: "About",
+    path: '/about',
+    name: 'About',
     component: About,
   },
-];
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
-});
+})
 
-export default router;
+export default router
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -2,6 +2,21 @@
   <div class="events">
     <h1>This is an event page</h1>
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <div class="pagination">
+      <router-link
+        :to="{ name: 'EventList', query: { page: page - 1 } }"
+        rel="prev"
+        v-if="page != 1"
+        >Prev Page</router-link
+      >
+
+      <router-link
+        :to="{ name: 'EventList', query: { page: page + 1 } }"
+        rel="next"
+        v-if="hasNextPage"
+        >Next Page</router-link
+      >
+    </div>
   </div>
 </template>
 
@@ -9,25 +24,41 @@
 // @ is an alias to /src
 import EventCard from '@/components/EventCard.vue'
 import EventService from '@/services/EventService.js'
+import { watchEffect } from 'vue'
 
 export default {
-  name: 'Home',
+  name: 'EventList',
+  props: ['page'],
   components: {
     EventCard,
   },
   data() {
     return {
       events: null,
+      totalEvents: 0,
     }
   },
   created() {
-    EventService.getEvents()
-      .then((response) => {
-        this.events = response.data
-      })
-      .catch((error) => {
-        console.log(error)
-      })
+    watchEffect(() => {
+      this.events = null
+      EventService.getEvents(2, this.page)
+        .then((response) => {
+          this.events = response.data
+          this.totalEvents = response.headers['x-total-count']
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    })
+  },
+  computed: {
+    hasNextPage() {
+      // First, calculate total pages
+      var totalPages = Math.ceil(this.totalEvents / 2) // 2 is events per page
+
+      // Then check to see if the current page is less than the total pages.
+      return this.page < totalPages
+    },
   },
 }
 </script>
@@ -37,5 +68,10 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+
+.pagination {
+  display: flex;
+  justify-content: space-between;
 }
 </style>
