a9cb35e47eedd2d9b3f9f62733645898dc116601
Jessica Schäfer <jschaefer@3pc.de>
2021-11-07 20:24:34 +0100

    feat(router): change savedPosition behavior

--- src/router/index.js
+++ src/router/index.js
@@ -74,6 +74,13 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { top: 0 };
+    }
+  },
 });
 
 export default router;
