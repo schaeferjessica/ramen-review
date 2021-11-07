38d0400d1e1eb4e9fcaad6b3e2c408e3823e0ca0
Jessica Schäfer <jschaefer@3pc.de>
2021-11-07 18:53:47 +0100

    feat(NotFound): add Error Handling and 404

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,8 +1,17 @@
 <template>
   <p>Register for the event here</p>
+  <button @click="register">Register me</button>
 </template>
 <script>
 export default {
   props: ['event'],
+  methods: {
+    register() {
+      // If registration API call is successful
+      this.$router.push({
+        name: 'EventDetails',
+      })
+    },
+  },
 }
 </script>
